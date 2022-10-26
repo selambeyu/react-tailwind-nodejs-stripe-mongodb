29b54387991f1d12cb449c8d4c1f6a3a0120516d
melkam_beyene <melkam3392beyu@gmail.com>
2022-10-26 16:01:57 +0300

    :star: add registration page

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
 import "./App.css";
 // import Home from './pages/Home'
 import Nav from "./component/layout/Nav";
-import Login from "./pages/Login";
+// import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
   return (
     <div>
       <Nav />
-      <Login />
+      <Register />
     </div>
   );
 }
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -16,12 +16,6 @@ function Login() {
             <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
               Sign in to your account
             </h2>
-            <p className="mt-2 text-center text-sm text-gray-600">
-              Or{" "}
-              <Link className="font-medium text-indigo-600 hover:text-indigo-500">
-                start your 14-day free trial
-              </Link>
-            </p>
           </div>
           <form className="mt-8 space-y-6" action="#" method="POST">
             <input type="hidden" name="remember" defaultValue="true" />
